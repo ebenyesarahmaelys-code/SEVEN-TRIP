fd78b2c21159a9e7dfdbe25344661fc1eabce281
EBENYE <ebenyesarahmaelys@gmail.com>
2026-07-22 00:20:55 +0100

    Correction bug : correspondance entre les variables du html inscription et du JS

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
 function inscription() {
-
-    
     let nom = document.getElementById("nom").value;
     let prenom = document.getElementById("prenom").value;
     let email = document.getElementById("email").value;
-    let tel = document.getElementById("tel").value;
     let utilisateur = document.getElementById("utilisateur").value;
     let mdp = document.getElementById("mdp").value;
-    let mdpConf =document.getElementById("mdp-conf").value;
-
+    let mdpConf = document.getElementById("mdp-conf").value;
 
     if (!nom || !prenom || !email || !utilisateur || !mdp || !mdpConf) {
         alert("Veuillez remplir tous les champs.");
         return;
     }
 
-
     if (mdp !== mdpConf) {
         alert("Les mots de passe ne correspondent pas.");
         return;
-    } 
-
+    }
 
     let utilisateurs = JSON.parse(localStorage.getItem("utilisateurs")) || [];
-    let existe = utilisateur.some(u => u.utilisateur === utilisateur);
+    let existe = utilisateurs.some(u => u.utilisateur === utilisateur);
+
     if (existe) {
-        alert("Ce nom dutilisateur est deja pris.")
+        alert("Ce nom d'utilisateur est déjà pris.");
         return;
     }
 
@@ -33,7 +28,6 @@ function inscription() {
         nom: nom,
         prenom: prenom,
         email: email,
-        tel: tel,
         utilisateur: utilisateur,
         mdp: mdp,
         points: 0
@@ -42,6 +36,8 @@ function inscription() {
     utilisateurs.push(nouvelUtilisateur);
     localStorage.setItem("utilisateurs", JSON.stringify(utilisateurs));
 
-    alert ("Compte créé avec succes !Bienvenue" + nom +"☺️");
+    alert("Compte créé avec succès ! Bienvenue " + nom + " ☺️");
 
+    // Rediriger vers la page d'accueil
+    window.location.href = "Accueil.html";
 }
