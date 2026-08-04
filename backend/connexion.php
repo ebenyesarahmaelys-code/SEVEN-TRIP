515f6f868ce6e7e9afb29451f8db7613d327afcf
EBENYE <ebenyesarahmaelys@gmail.com>
2026-08-04 17:37:41 +0100

    Function pour se connecter: correction erreur dans la function

--- backend/connexion.php
+++ backend/connexion.php
@@ -1,24 +1,20 @@
-<?php 
-include 'config.php'
-// connexion 
+<?php
+include 'config.php';
+// Connexion
 function connecter($utilisateur, $mdp) {
     global $conn;
-
-    //chercher lutilisateur dans la base de donees
-
-    $sql ="SELECT * FROM utilisateurs WHERE utilisateur = '$utilisateur'";
+// On cherche lutilisateur 
+    $sql = "SELECT * FROM utilisateurs WHERE utilisateur = '$utilisateur'";
     $result = $conn->query($sql);
-
-    // ce quon fait si il existe 
-    if($result->num_rows > 0) {
+// une fois trouver on va verifier si le mot de passe correspond
+    if ($result->num_rows > 0) {
         $user = $result->fetch_assoc();
-        
-        //si il existe on verifie le mot de passe 
-        if(password_verify($mdp, $user['mdp'])) {
-            return["sucess"=> true, "user" => $user];
+
+        if (password_verify($mdp, $user['mdp'])) {
+            return ["success" => true, "user" => $user];
         }
     }
 
     return ["success" => false, "message" => "Identifiants incorrects."];
-
 }
+?>
